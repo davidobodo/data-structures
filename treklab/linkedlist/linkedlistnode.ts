88b41337b8cf59445aeb6d93020f4640352e257c
David Obodo <obododavid5@gmail.com>
2020-07-02 02:46:55 +0100

    feat: create linkedlist node

--- treklab/linkedlist/linkedlistnode.ts
+++ treklab/linkedlist/linkedlistnode.ts
@@ -1,6 +1,13 @@
 class LinkedlistNode {
-    constructor(value, next = null) {
-        this.value = value
-        this.next = next
+    public value: number;
+    public next: string;
+
+    constructor(theValue: number, theNext = null) {
+        this.value = theValue
+        this.next = theNext
+    }
+
+    toString(callback: (val: number) => void) {
+        return callback ? callback(this.value) : `${this.value}`
     }
 }
